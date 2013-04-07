ba0383346589b8f93c8d8adfa4c89473c8ef2dae
amartira <andresmrazola@gmail.com>
2013-04-07 17:09:01 +0200

    otra prueba

--- src/com/example/bolas/MainActivity.java
+++ src/com/example/bolas/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	String a = "ww";
         super.onCreate(savedInstanceState);
         // Creamos la nueva instancia de la clase, y le enviamos como contexto
         // nuestra actividad
